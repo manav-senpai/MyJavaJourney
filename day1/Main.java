ceb2547952efb0632693a360e168c9e208e6b749
Manav Ahire <rockstarsigma@gmail.com>
2025-08-31 21:51:13 +0530

    Add files via upload

---
+++ day1/Main.java
@@ -0,0 +1,36 @@
+public class Main{
+    public static void main(String[] args){
+//    double quantity = 23.5;
+//        System.out.println(quantity);
+//    char grade = 'a';
+//    char symbol = '#';
+//        System.out.print(grade +" "+ symbol);
+//
+//        boolean isStudent = true;
+//        if (isStudent){
+//            System.out.println("You are a student");
+//        }
+//        else {
+//            System.out.println("You are not a student");
+//        }
+//
+//        System.out.println(isStudent);
+//        String name = "Manav ahire";
+//        System.out.println("Hello "+name);
+//        System.out.println("Your name is "+name+ " and you love programming");
+
+        // System.out.println("assignment it is..");
+        String name = "Bro Code";
+        int gta = 6;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+        System.out.println(name);
+        System.out.println(gta);
+        System.out.println(pi);
+        System.out.println(gender);
+        System.out.println(isAdmin);
+
+    }
+}
